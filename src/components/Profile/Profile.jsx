dc368b9ce141fcabd597c3956e37d434bdb92c59
ALPOFF <leha.mms82@gmail.com>
2019-09-19 13:23:59 +0300

    styles

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import classes from './Profile.module.css'
 import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
-    return <div className={classes.content}>
+    return <div>
         <div>
             <img src={'http://s3.amazonaws.com/wallpapers2/wallpapers/images/000/000/582/thumb/andromeda_galaxy.jpg?1499899637'}/>
         </div>
