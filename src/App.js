7e44bebbc1beee222f9ab8c988d9a8820f748095
ALPOFF <leha.mms82@gmail.com>
2019-09-19 16:05:21 +0300

    19

--- src/App.js
+++ src/App.js
@@ -4,18 +4,23 @@ import Header from './components/Header/Header'
 import Nav from "./components/Nav/Nav";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 
 const App = () =>  {
     return (
+        <BrowserRouter>
         <div className={'app-wrapper'}>
           <Header />
           <Nav/>
           <div class={'app-wrapper-content'}>
-              <Dialogs/>
+              <Route path = '/dialogs' component = {Dialogs}/>
+              <Route path = '/profile' component = {Profile}/>
           </div>
           {/*<Profile/>*/}
         </div>
-    );
+        </BrowserRouter>
+
+            );
 }
 
 
