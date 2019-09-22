4fb9e035aec4aea70edc7daf8d383ef148017ee0
ALPOFF <leha.mms82@gmail.com>
2019-09-22 21:44:54 +0300

    deleted methods

--- src/redux/state.js
+++ src/redux/state.js
@@ -30,22 +30,7 @@ let store = {
     subscribe(observer) {
         this._callSubscriber = observer;
     },
-
-    addPost() {
-        let newPost = {
-            id: 5,
-            message: this._state.profilePage.newPostText,
-            likesCount: 0
-        };
-
-        this._state.profilePage.postData.push(newPost);
-        this._state.profilePage.newPostText = '';
-        this._callSubscriber(this._state)
-    },
-    updateNewPostText(newText) {
-        this._state.profilePage.newPostText = newText;
-        this._callSubscriber(this._state)
-    },
+    
     dispatch(action) {
         if (action.type === 'ADD-POST') {
             let newPost = {
