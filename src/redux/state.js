7d8df92114adfe9c0272f6d1237b2baf610252c5
ALPOFF <leha.mms82@gmail.com>
2019-09-22 18:08:42 +0300

    lesson 33

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,5 @@
+import {rerenderEntireTree} from "../render";
+
 let state = {
     profilePage: {
         postData: [
@@ -28,6 +30,7 @@ export let addPost = (postMessage) => {
     };
 
     state.profilePage.postData.push(newPost);
+    rerenderEntireTree(state)
 }
 
 export default state;
