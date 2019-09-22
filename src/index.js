86c6568bc103fa48bd4334808aced7e626771a45
ALPOFF <leha.mms82@gmail.com>
2019-09-22 19:52:41 +0300

    lesson 35

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './index.css';
+import state, {subscribe} from "./redux/state";
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {rerenderEntireTree} from "./render"
-import state from "./redux/state";
+import {addPost, updateNewPostText} from "./redux/state";
+
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(<App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />, document.getElementById('root'));
+}
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
 
 rerenderEntireTree(state);
 
+subscribe(rerenderEntireTree);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
