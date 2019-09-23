b32cb8c387d9a637e36d0d8c1477c516dc90629f
ALPOFF <leha.mms82@gmail.com>
2019-09-24 00:33:56 +0300

    func in reducers

--- src/redux/profiles-reducer.js
+++ src/redux/profiles-reducer.js
@@ -21,4 +21,18 @@ const profilesReducer = (state, action) => {
 
 }
 
+export const addPostActionCreator = () => {
+    return {
+        type: ADD_POST
+    }
+}
+
+export const updateNewPostTextActionCreator = (text) => {
+    return {
+        type: UPDATE_NEW_POST_TEXT, newText: text
+    }
+}
+
+
+
 export default profilesReducer;
