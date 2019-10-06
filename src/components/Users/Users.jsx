228d53ddef0c37d54147f165f9b3b259fa3e42bd
ALPOFF <leha.mms82@gmail.com>
2019-10-06 19:14:43 +0300

    lesson 52
    side effect

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,23 +2,33 @@ import React from "react";
 import * as axios from "axios";
 
 let Users = (props) => {
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items)
-        });
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items)
+            });
+        }
     }
 
+
     return <div>
+        <button onClick={getUsers}>GetUsers</button>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photos.small != null ? u.photos.small : "http://bootstraptema.ru/snippets/icons/2016/mia/3.png" } alt="" height={50}/>
+                        <img
+                            src={u.photos.small != null ? u.photos.small : "http://bootstraptema.ru/snippets/icons/2016/mia/3.png"}
+                            alt="" height={50}/>
                     </div>
                     <div>
                         {u.followed
-                            ? <button onClick={ () => { props.unfollow(u.id) } }>Unfollow</button>
-                            : <button onClick={ () => { props.follow(u.id) } }>Follow</button>}
+                            ? <button onClick={() => {
+                                props.unfollow(u.id)
+                            }}>Unfollow</button>
+                            : <button onClick={() => {
+                                props.follow(u.id)
+                            }}>Follow</button>}
                     </div>
                 </span>
                 <span>
