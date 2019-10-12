9225851bdc74df6e0d35f80c25b7bbb11bd6f2da
ALPOFF <leha.mms82@gmail.com>
2019-10-12 21:45:50 +0300

    lesson 61
    cookie, login
    auth/me

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import classes from './Header.module.css'
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return <header className={classes.header}>
         <img src='https://seeklogo.com/images/C/company-name-logo-09881CAD1A-seeklogo.com.png'/>
+
+        <div className={classes.loginBlock}>
+            {props.isAuth ? props.login
+                :<NavLink to={'/login/'}>Login</NavLink>}
+        </div>
     </header>
 }
 
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -2,13 +2,17 @@ import {combineReducers, createStore} from "redux";
 import profilesReducer from "./profiles-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import usersReducer from "./users-reducer";
+import authReducer from "./auth-reducer";
 
 let reducers = combineReducers({
     profilePage: profilesReducer,
     messagesPage: dialogsReducer,
-    usersPage: usersReducer
+    usersPage: usersReducer,
+    auth: authReducer
 });
 
 let store = createStore(reducers);
+
+window.store = store;
 
 export default store;
