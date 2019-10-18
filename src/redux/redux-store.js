f68912092320d7044a46332fd678abc20c033d43
ALPOFF <leha.mms82@gmail.com>
2019-10-18 20:24:48 +0300

    lesson 75
    redux-form

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -4,12 +4,14 @@ import dialogsReducer from "./dialogs-reducer";
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import thunkMiddleware from "redux-thunk" //import thunkmiddleware
+import { reducer as formReducer } from 'redux-form'
 
 let reducers = combineReducers({
     profilePage: profilesReducer,
     messagesPage: dialogsReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware)); //second param - middlw for thunk
